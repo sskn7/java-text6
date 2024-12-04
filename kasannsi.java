4985fe57ecf4f7d2f862f39156ce6a2209cfea2d
kana sasazawa <kana.yellow7@gmail.com>
2024-12-04 22:36:40 +0900

    前置減算子

--- kasannsi.java
+++ kasannsi.java
@@ -2,17 +2,26 @@
 public class kasannsi {
 
 	public static void main(String[] args) {
+		//前置加算子
 		int a = 10;
 		System.out.println(++a);
 		System.out.println(++a);
 		System.out.println(++a);
 		System.out.println(++a);
 		
+		//後置加算子
 		int b = 10;
 		System.out.println(b++);
 		System.out.println(b++);
 		System.out.println(b++);
 		System.out.println(b++);
+		
+		//前置減算子
+		int c = 10;
+		System.out.println(--c);
+		System.out.println(--c);
+		System.out.println(--c);
+		System.out.println(--c);
 		
 	}
 
